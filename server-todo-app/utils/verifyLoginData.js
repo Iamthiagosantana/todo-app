8b4b03aa68e288611177fc37957c0755b99e874c
Thiago de Santana Silva <othiago_10_ago@hotmail.com>
2023-10-15 21:43:15 -0300

    Refactored login data

--- server-todo-app/utils/verifyLoginData.js
+++ server-todo-app/utils/verifyLoginData.js
@@ -1,11 +1,11 @@
-const disallowedCharacters = /^[a-zA-Z0-9]+$/
+const allowedCharacters = /^[a-zA-Z0-9]+$/
 
 module.exports = (username, password) => {
 
     if ((username.length < 6 || username.length > 20) || (password.length < 8 || password.length > 20)) {
         return false
     }
-    if (!disallowedCharacters.test(username) || !disallowedCharacters.test(password)) {
+    if (!allowedCharacters.test(username) || !allowedCharacters.test(password)) {
         return false
     }
     
